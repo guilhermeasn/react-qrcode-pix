ee17c0bc6427799330ece6f68e2e691d84b75958
Guilherme Augusto <guilhermeasn@yahoo.com.br>
2022-03-06 11:31:34 -0300

    explained

--- src/App.js
+++ src/App.js
@@ -13,6 +13,20 @@ function App() {
     
         <h1>Exemplos React Qrcode PIX</h1>
 
+        <hr />
+
+        <h3>Minimal Configuration</h3>
+
+        <code>{`
+            <PIX
+                pixkey='guilhermeasn@yahoo.com.br'
+                merchant='Guilherme Neves'
+                city='Paraíba do Sul'
+
+                onLoad={ payload => setMinimalPIX(payload) }
+            />
+        `}</code>
+
         <PIX
             pixkey='guilhermeasn@yahoo.com.br'
             merchant='Guilherme Neves'
@@ -22,6 +36,23 @@ function App() {
         />
 
         <p>{ minimalPIX }</p>
+
+        <hr />
+
+        <h3>Fully configuration</h3>
+
+        <code>{`
+            <PIX
+                pixkey='guilhermeasn@yahoo.com.br'
+                merchant='Guilherme Neves'
+                city='Paraíba do Sul'
+                cep='25.850-000'
+                code={ 'RQP' + now }
+                amount={ 100 }
+
+                onLoad={ payload => setFullPIX(payload) }
+            />
+        `}</code>
 
         <PIX
             pixkey='guilhermeasn@yahoo.com.br'
